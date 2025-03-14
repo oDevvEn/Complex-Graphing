e91d0b6caf4489d2de4546bf991fdd81ccaf4b18
oDevvEn <69240278+oDevvEn@users.noreply.github.com>
2025-03-14 20:53:08 +0000

    yayy this works for me at least! now to never touch this project ever again

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 54.5 whole lines i can see on a 15" 1366x768 display!
